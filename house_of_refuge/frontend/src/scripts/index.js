f077d11f622b773f3638f6e8049442e0686e1293
Tadek Teleżyński <tadekte@gmail.com>
2022-02-28 18:22:26 +0100

    add search and filter capabilities

--- house_of_refuge/frontend/src/scripts/index.js
+++ house_of_refuge/frontend/src/scripts/index.js
@@ -2,9 +2,10 @@ import "../styles/resources.scss";
 import React, {useEffect, useState} from 'react'; // eslint-disable-line
 import ReactDOM from 'react-dom';
 import {Button, Table} from "react-bootstrap";
-import {SortUp, SortDown} from "react-bootstrap-icons";
+import {SortUp, SortDown, Filter, Search} from "react-bootstrap-icons";
 import BootstrapSwitchButton from 'bootstrap-switch-button-react';
 import {orderBy} from "lodash";
+import Select from "react-dropdown-select";
 
 
 const ResourceRow = ({resource, isExpanded}) => {
@@ -56,12 +57,27 @@ const ResourceRow = ({resource, isExpanded}) => {
 };
 
 
-const ColumnHeader = ({col, sortHandler, isSorting, sortDirection}) => {
+const ColumnHeader = ({col, sortHandler, isSorting, sortDirection, filterData}) => {
   const iconClass = isSorting ? "sort-active" : "sort-muted";
+  const [showFilter, setShowFilter] = useState(false);
+
   return <div className={"col-head col"}>
-    {col.display} {sortDirection === "asc" ?
-      <SortUp className={iconClass} onClick={() => sortHandler(col.fieldName)}/> : <SortDown
-          className={iconClass} onClick={() => sortHandler(col.fieldName)}/>}
+    <div className={'top'}>
+      {col.display} {sortDirection === "asc" ?
+        <SortUp className={iconClass} onClick={() => sortHandler(col.fieldName)}/> : <SortDown
+            className={iconClass} onClick={() => sortHandler(col.fieldName)}/>}
+      {filterData && <Filter style={{cursor: 'pointer'}} onClick={() => setShowFilter(f => !f)}/>}
+    </div>
+    {filterData &&
+        <div className={'filter'}>
+          <Select
+              className={showFilter ? "" : "hide"}
+              multi
+              options={[...filterData.options].map(o => ({label: o, value: o}))}
+              onChange={filterData.handler}
+          />
+        </div>
+    }
   </div>;
 };
 
@@ -69,6 +85,8 @@ const ResourceList = ({resources}) => {
   const [visibleResources, setVisibleResources] = useState(resources);
   const [onlyWarsaw, setOnlyWarsaw] = useState(false);
   const [onlyAvailable, setOnlyAvailable] = useState(false);
+  const [peopleFilter, setPeopleFilter] = useState(null);
+  const [searchQuery, setSearchQuery] = useState("");
   const [sortBy, setSortBy] = useState("name");
   const [sortOrder, setSortOrder] = useState("asc");
   const [expandAll, setExpandAll] = useState(false);
@@ -81,6 +99,10 @@ const ResourceList = ({resources}) => {
     accommodation_length: {fieldName: 'accommodation_length', display: "Na jak długo?", sort: "asc"},
 
   });
+
+  const resourceAsString = (r) => {
+    return Object.values(r).join(' ').toLowerCase();
+  };
 
   const handleSort = (column) => {
     setSortBy(column);
@@ -99,14 +121,25 @@ const ResourceList = ({resources}) => {
     setVisibleResources(
         resources.filter(
             r => onlyWarsaw ? isInWarsaw(r) : true
-        ).filter(r => onlyAvailable ? isAvailable(r) : true)
+        ).filter(
+            r => onlyAvailable ? isAvailable(r) : true
+        ).filter(
+            r => peopleFilter ? peopleFilter.includes(r.people_to_accommodate) : true
+        ).filter(
+            r => searchQuery ? resourceAsString(r).search(searchQuery) > -1 : true
+        )
     );
-  }, [onlyWarsaw, onlyAvailable]);
+  }, [onlyWarsaw, onlyAvailable, peopleFilter, searchQuery]);
   useEffect(() => {
     setVisibleResources(
         vr => orderBy(vr, [sortBy], [sortOrder])
     );
   }, [sortBy, sortOrder]);
+
+  const peopleFilterData = {
+    handler: (values) => values.length ? setPeopleFilter(values.map(v => v.value)) : setPeopleFilter(null),
+    options: new Set(resources.map(r => r.people_to_accommodate))
+  };
 
   return (
       <>
@@ -132,16 +165,25 @@ const ResourceList = ({resources}) => {
                     setOnlyAvailable(checked);
                   }}
               /></td>
+            <td>
+              <Search />
+              <input className="search-input" onChange={
+                (e) => setSearchQuery(e.target.value.toLowerCase())
+              } />
+            </td>
+
           </tr>
           </tbody>
         </Table>
 
         {/*<Button>Do odbioru dzisiaj</Button>*/}
         {/*<Button>Na terenie warszawy</Button>*/}
+        <div><p>{`${visibleResources.length} zasóbów`}</p></div>
         <div className={"column-headers mt-3"}>
           {Object.values(columnsData).map(colData => <
               ColumnHeader col={colData} key={colData.fieldName} sortHandler={handleSort}
                            sortDirection={sortOrder} isSorting={sortBy === colData.fieldName}
+                           filterData={colData.fieldName === "people_to_accommodate"? peopleFilterData : null}
           />)}
         </div>
         {visibleResources.map(r => <ResourceRow resource={r} isExpanded={expandAll} key={r.id}/>)}
