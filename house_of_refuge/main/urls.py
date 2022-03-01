70211621230872b7f6de5d66afb518157e57d490
Tadek Teleżyński <tadekte@gmail.com>
2022-03-01 17:48:39 +0100

    tadek wip

--- house_of_refuge/main/urls.py
+++ house_of_refuge/main/urls.py
@@ -6,4 +6,8 @@ app_name = "main"
 urlpatterns = [
     path("", views.resource_gathering, name="main_form"),
     path("zasoby", views.housing_list, name="zasoby"),
+    path("api/update_status/<int:resource_id>", views.update_resource_status, name="status_update"),
+    path("api/zasoby", views.get_resources, name="zasoby_get"),
+    path("api/zgloszenia", views.get_submissions, name="zgloszenia_get"),
+    path("api/zglos", views.create_submission, name="zgloszenie_create"),
 ]
