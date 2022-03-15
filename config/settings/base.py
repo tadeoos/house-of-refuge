37daae40836343f85433b4e115910de5f14b1143
Tadek Teleżyński <tadekte@gmail.com>
2022-03-15 14:00:56 +0100

    change admin email to mine

--- config/settings/base.py
+++ config/settings/base.py
@@ -240,7 +240,7 @@ EMAIL_TIMEOUT = 5
 # Django Admin URL.
 ADMIN_URL = "admin/"
 # https://docs.djangoproject.com/en/dev/ref/settings/#admins
-ADMINS = [("""Tadek Teleżyński""", "tadek-teleżyński@example.com")]
+ADMINS = [("""Tadek Teleżyński""", "tadekte@gmail.com")]
 # https://docs.djangoproject.com/en/dev/ref/settings/#managers
 MANAGERS = ADMINS
 
