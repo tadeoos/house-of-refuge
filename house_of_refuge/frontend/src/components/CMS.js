fc9fcaaf79310d858f8844226c0cce325d035d3b
galczo5 <kamil.galek@gmail.com>
2022-03-19 15:25:30 +0100

    cms ready

--- house_of_refuge/frontend/src/components/CMS.js
+++ house_of_refuge/frontend/src/components/CMS.js
@@ -1,34 +1,78 @@
 import React, {useEffect, useState} from "react";
-import TextWrapper from '../typography/TextWrapper';
-import H5 from '../typography/H5';
 import {useParams} from "react-router-dom";
+import axios from "axios";
+import LangSwitch, {DEFAULT_LANG, LANGS} from "./LangSwitch";
+
+export class CmsResource {
+
+    static instance;
+
+    data = [];
+
+    static getInstance() {
+        if (!this.instance) {
+            this.instance = new CmsResource();
+        }
+
+        return this.instance;
+    }
+
+    fetch() {
+        return axios.get('/api/menu_pages')
+            .then(response => {
+                this.data = response.data;
+                return this.data;
+            });
+    }
+
+    isFetched() {
+        return this.data.length > 0;
+    }
+
+    getData() {
+        if (!this.isFetched()) {
+            return this.fetch();
+        }
+
+        return new Promise(resolve => resolve(this.data));
+    }
+
+}
 
 
 const CMS = () => {
     const params = useParams();
+    const [page, setPage] = useState(null);
+    const [lang, setLang] = useState(DEFAULT_LANG);
 
-    const [titlePrimary, setTitlePrimary] = useState('');
-    const [contentPrimary, setContentPrimary] = useState('');
-
-    const [titleSecondary, setTitleSecondary] = useState('');
-    const [contentSecondary, setContentSecondary] = useState('');
 
     useEffect(() => {
-        setTitlePrimary('Title');
-        setTitleSecondary('Title');
-
-        setContentPrimary('Content');
-        setContentSecondary('Content');
+        CmsResource.getInstance().getData()
+            .then(data => {
+                const cmsPage = data.find(d => d.slug === params.id);
+                if (cmsPage) {
+                    setPage(cmsPage);
+                }
+            });
     });
 
+    function toggleText(lang, primary, secondary) {
+        return lang === DEFAULT_LANG ? primary : (secondary ?? primary);
+    }
+
+    const langs = page && page.content_secondary_language ? LANGS : [DEFAULT_LANG];
+
     return (
-        <TextWrapper>
-            <H5> { titlePrimary } / {titleSecondary}<br /></H5>
-            <div className={"d-flex gap-5"}>
-                <div className={"flex-grow-1"} dangerouslySetInnerHTML={{ __html: contentPrimary }}></div>
-                <div className={"flex-grow-1"} dangerouslySetInnerHTML={{ __html: contentSecondary }}></div>
+        page &&
+        <div>
+            { langs.length > 1 && <div className={"d-flex justify-content-end"}>
+                <LangSwitch languages={langs} currentLang={lang} changeLang={(x) => setLang(x)}/>
+            </div> }
+            <div className={"d-flex gap-5 w-100"}>
+                <div className={"flex-grow-1"}
+                     dangerouslySetInnerHTML={{__html: toggleText(lang, page.content_primary_language, page.content_secondary_language)}}></div>
             </div>
-        </TextWrapper>
+        </div>
     );
 };
 
--- house_of_refuge/frontend/src/components/Menu.js
+++ house_of_refuge/frontend/src/components/Menu.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 import LogoFB from './LogoFB.js';
 import MenuIcon from './MenuIcon.js';
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import TextMultiLang from '../components/TextMultiLang';
 import useComponentVisible from '../scripts/useComponentVisible';
+import {useEffect, useState} from "react";
+import {CmsResource} from "./CMS";
 
 const StyledLink = styled(Link)`
   text-decoration: none;
@@ -15,77 +17,87 @@ const StyledLink = styled(Link)`
   transition: all 0.3s;
 
   :hover {
-      background-color: #EDEDED;
-      color: inherit;
-   }
+    background-color: #EDEDED;
+    color: inherit;
+  }
 
 `;
 
 const Button = styled.button`
-    position: fixed;
-    top: 5px;
-    right: 52px;
-    border: none;
-    padding: 0;
-    margin: 0;
-    background-color: transparent;
-    transition: all 0.3s;
+  position: fixed;
+  top: 5px;
+  right: 52px;
+  border: none;
+  padding: 0;
+  margin: 0;
+  background-color: transparent;
+  transition: all 0.3s;
 
-    :hover {
-        opacity: 0.7;
-    }
+  :hover {
+    opacity: 0.7;
+  }
 
-    @media (max-width: 600px) {
-     left: 10px;
-     right: initial;
-    }
+  @media (max-width: 600px) {
+    left: 10px;
+    right: initial;
+  }
 `;
 
 const ExtrernalLink = styled.a`
-    position: fixed;
-    top: 5px;
-    right: 16px;
-    border: none;
-    padding: 0;
-    margin: 0;
-    background-color: transparent;
-    transition: all 0.3s;
+  position: fixed;
+  top: 5px;
+  right: 16px;
+  border: none;
+  padding: 0;
+  margin: 0;
+  background-color: transparent;
+  transition: all 0.3s;
 
-    :hover {
-        opacity: 0.7;
-    }
+  :hover {
+    opacity: 0.7;
+  }
 
-    @media (max-width: 600px) {
-        right: 12px;
-    }
+  @media (max-width: 600px) {
+    right: 12px;
+  }
 `;
 
 
 const MiniMenu = styled.div`
-   position: fixed;
-   max-width: 220px;
-   background-color: white;
-   top: 42px;
-   right: 14px;
-   border-radius: 22px;
-   padding: 8px;
-   -webkit-box-shadow: 0px 5px 28px 0px rgba(0, 0, 0, 0.14);
-    -moz-box-shadow: 0px 5px 28px 0px rgba(0, 0, 0, 0.14);
-    box-shadow: 0px 5px 28px 0px rgba(0, 0, 0, 0.14);
+  position: fixed;
+  max-width: 220px;
+  background-color: white;
+  top: 42px;
+  right: 14px;
+  border-radius: 22px;
+  padding: 8px;
+  z-index: 1000;
+  -webkit-box-shadow: 0px 5px 28px 0px rgba(0, 0, 0, 0.14);
+  -moz-box-shadow: 0px 5px 28px 0px rgba(0, 0, 0, 0.14);
+  box-shadow: 0px 5px 28px 0px rgba(0, 0, 0, 0.14);
 
-    @media (max-width: 600px) {
-        left: 14px;
-    }
+  @media (max-width: 600px) {
+    left: 14px;
+  }
 
 `;
 
 
 const Menu = () => {
-    const { menu, button, miniMenuOpened, setMiniMenuOpened } = useComponentVisible(true);
+    const {menu, button, miniMenuOpened, setMiniMenuOpened} = useComponentVisible(true);
+
+    const [cmsPages, setCmsPages] = useState([]);
+
+    useEffect(() => {
+        if (miniMenuOpened) {
+            CmsResource.getInstance().getData()
+                .then(data => setCmsPages(data));
+        }
+    }, [miniMenuOpened]);
 
     return (
         <>
-            <Button ref={button} > <MenuIcon /> </Button>
+            <Button ref={button}> <MenuIcon/> </Button>
             {miniMenuOpened && <MiniMenu ref={menu}>
                 <StyledLink to="/privacy" onClick={() => setMiniMenuOpened(!miniMenuOpened)}>
                     <TextMultiLang
@@ -100,11 +112,12 @@ const Menu = () => {
                     />
                 </StyledLink>
 
-                { ['about-radom-city'].map((title) => {
-                    return <StyledLink to={"/page/" + title} onClick={() => setMiniMenuOpened(!miniMenuOpened)}>
+                {cmsPages.map((page) => {
+                    return <StyledLink key={page.slug} to={"/page/" + page.slug}
+                                       onClick={() => setMiniMenuOpened(!miniMenuOpened)}>
                         <TextMultiLang
-                            primaryText={title}
-                            secondaryText={title}
+                            primaryText={page.menu_title_primary_language}
+                            secondaryText={page.menu_title_secondary_language}
                         />
                     </StyledLink>;
                 })}
@@ -112,9 +125,9 @@ const Menu = () => {
             <ExtrernalLink
                 target="_blank"
                 rel="noopener noreferrer"
-                href='https://www.facebook.com/groups/zasobygrupa'
+                href="https://www.facebook.com/groups/zasobygrupa"
             >
-                <LogoFB />
+                <LogoFB/>
             </ExtrernalLink>
         </>
     );
