970bd73149460d06d93e6f228be6c5730e80e8c4
Tadek Teleżyński <tadekte@gmail.com>
2022-03-25 20:53:46 +0100

    add max width to cms pages

--- house_of_refuge/frontend/src/components/CMS.js
+++ house_of_refuge/frontend/src/components/CMS.js
@@ -69,7 +69,7 @@ const CMS = () => {
                 <LangSwitch languages={langs} currentLang={lang} changeLang={(x) => setLang(x)}/>
             </div> }
             <div className={"d-flex gap-5 w-100 px-4"}>
-                <div className={"flex-grow-1"}
+                <div className={"flex-grow-1"} style={{maxWidth: "800px"}}
                      dangerouslySetInnerHTML={{__html: toggleText(lang, page.content_primary_language, page.content_secondary_language)}}></div>
             </div>
         </div>
